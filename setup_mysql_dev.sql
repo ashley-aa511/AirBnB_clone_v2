9716d5fe04561b079d58322ff7a94e1b1f17f3e0
ashley-aa511 <okeyoashley@gmail.com>
2024-04-17 01:00:49 -0700

    sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- Create or use the hbnb_dev_db database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or update the hbnb_dev user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
